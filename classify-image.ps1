e2220a89e8cea4e8219056ae07fea658f91d366b
AND3FELIPE <afsilva6236@misena.edu.co>
2023-08-13 15:29:37 -0500

    Realizacion de ejercicio(CustomVision[animales-frutas])

--- classify-image.ps1
+++ classify-image.ps1
@@ -1,6 +1,5 @@
-$predictionUrl="https://csandreacustom.cognitiveservices.azure.com/customvision/v3.0/Prediction/441eace9-0db4-4b51-b15d-ef65e8b82fff/classify/iterations/Iteration1/url"
+$predictionUrl = "https://csandreacustom.cognitiveservices.azure.com/customvision/v3.0/Prediction/441eace9-0db4-4b51-b15d-ef65e8b82fff/classify/iterations/Iteration1/url"
 $predictionKey = "299096d5acf840d6b4306a100018b32c"
-
 
 # Code to call Custom Vision service for image classification
 
@@ -13,17 +12,33 @@ if ($args.count -gt 0 -And $args[0] -in (1..84))
 $img = "https://raw.githubusercontent.com/Gilaufen/Sesion1/main/custom/foto-$($img_num).jpg"
 
 $headers = @{}
-$headers.Add( "Prediction-Key", $predictionKey )
-$headers.Add( "Content-Type","application/json" )
+$headers.Add("Prediction-Key", $predictionKey)
+$headers.Add("Content-Type", "application/json")
 
 $body = "{'url' : '$img'}"
 
-write-host "Analyzing image..."
+Write-Host "Analyzing image..."
 $result = Invoke-RestMethod -Method Post `
-          -Uri $predictionUrl `
-          -Headers $headers `
-          -Body $body | ConvertTo-Json -Depth 5
+        -Uri $predictionUrl `
+        -Headers $headers `
+        -Body $body | ConvertTo-Json -Depth 5
 
-$prediction = $result | ConvertFrom-Json
+if ($result -ne $null) {
+    $prediction = $result | ConvertFrom-Json
+    if ($prediction.predictions.Count -gt 0) {
+        $tagName = $prediction.predictions[0].tagName
+        #Write-Host ("`n",$tagName, "`n")
+    } else {
+        Write-Host "No predictions found."
+    }
+} else {
+    Write-Host "No response from the service."
+}
 
-Write-Host ("`n",$prediction.predictions[0].tagName, "`n")
+if($tagName -match "banana|apple|orange"){
+    $fruiName = $tagName
+    Write-Host("Es una fruta: $fruiName")
+}else{
+    $animalName = $tagName
+    Write-Host("Es un animal: $animalName")
+}
