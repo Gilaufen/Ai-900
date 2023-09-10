623b875213aa74871df1d57ea98aa3b16ba1bd1f
Gilaufen <131225939+Gilaufen@users.noreply.github.com>
2023-09-10 12:41:36 -0500

    Update script.js Face API

--- Pruebas previas/Giselle/face_web/script.js
+++ Pruebas previas/Giselle/face_web/script.js
@@ -1,25 +1,16 @@
-  document.addEventListener("DOMContentLoaded", function () {
-  const analyzeButton = document.getElementById("analyzeButton");
-  const imageUrlImg = document.getElementById("imageUrl");
-  const resultDiv = document.getElementById("result");
-  const coordenadasDiv = document.getElementById("coordenadas");
+document.addEventListener("DOMContentLoaded", function () {
+    const analyzeButton = document.getElementById("analyzeButton");
+    const imageUrlImg = document.getElementById("imageUrl");
+    const resultDiv = document.getElementById("result");
+    const coordenadasDiv = document.getElementById("coordenadas");
 
-  analyzeButton.addEventListener("click", analyzeImage);
-  analyzeButton.addEventListener("click", cargarImagen);
+    analyzeButton.addEventListener("click", analyzeImage);
+    analyzeButton.addEventListener("click", cargarImagen);
 
-  function cargarImagen() {
-    const imagen = document.getElementById("imagenObjeto");
-    const link = document.getElementById("imageUrl").value;
-    imagen.src = link;
-  }
-
-  function analyzeImage() {
-    const Url = "https://carasandre.cognitiveservices.azure.com/";
-    const key = "947a7691ece046a8b2647074a8b0ee66";
-    const imageUrl = imageUrlImg.value;
-
-    if (!imageUrl) {
-      resultDiv.innerHTML = "Por favor ingrese una url...";
+    function cargarImagen() {
+        const imagen = document.getElementById("imagenObjeto");
+        const link = document.getElementById("imageUrl").value;
+        imagen.src = link;
     }
 
     function analyzeImage() {
@@ -27,27 +18,30 @@
         const key = "KEY   ";
         const imageUrl = imageUrlImg.value;
 
-    const body = JSON.stringify({ url: imageUrl });
+        if (!imageUrl) {
+            resultDiv.innerHTML = "Por favor ingrese una url...";
+        }
 
-    fetch(
-      `${Url}/face/v1.0/detect?returnfaceRecangle&detectionModel=detection_01`,
-      {
-        method: "POST",
-        headers: header,
-        body: body,
-      }
-    )
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result[0].faceRectangle);
+        const body = JSON.stringify({ url: imageUrl });
 
-        coordenadasDiv.style.height = `${result[0].faceRectangle.height}px`;
-        coordenadasDiv.style.width = `${result[0].faceRectangle.width}px`;
-        coordenadasDiv.style.top = `${result[0].faceRectangle.top}px`;
-        coordenadasDiv.style.left = `${result[0].faceRectangle.left}px`;
-      });
-  }
-  };
+        fetch(
+            `${Url}/face/v1.0/detect?returnfaceRecangle&detectionModel=detection_01`,
+            {
+                method: "POST",
+                headers: header,
+                body: body,
+            }
+        )
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result[0].faceRectangle);
+
+                coordenadasDiv.style.height = `${result[0].faceRectangle.height}px`;
+                coordenadasDiv.style.width = `${result[0].faceRectangle.width}px`;
+                coordenadasDiv.style.top = `${result[0].faceRectangle.top}px`;
+                coordenadasDiv.style.left = `${result[0].faceRectangle.left}px`;
+            });
+    }
 })
 
 // https://i.pinimg.com/originals/37/ea/96/37ea96c0d0ca2480a0084a2bd902bf67.jpg
