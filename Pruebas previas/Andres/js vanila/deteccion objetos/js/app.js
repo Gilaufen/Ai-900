058af1241f51dc25e5a4e5aa1bce0594eb41c7ea
AND3FELIPE <afsilva6236@misena.edu.co>
2023-09-11 12:52:55 -0500

    NEW : initial structure for object detection exercise

---
+++ Pruebas previas/Andres/js vanila/deteccion objetos/js/app.js
@@ -0,0 +1,8 @@
+console.log('perro');
+const boton = document.getElementById('button');
+
+boton.addEventListener('click', (message) => {
+  message = 'Que se dice madrecita'
+  console.log(message);
+})
+
