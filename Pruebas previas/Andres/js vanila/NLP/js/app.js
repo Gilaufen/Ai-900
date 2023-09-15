e940e0cea5331de801c9897cb2f1b28411a3521b
AND3FELIPE <afsilva6236@misena.edu.co>
2023-09-15 15:40:42 -0500

    DONE : ejercicico de traductor usando NPL terminado

--- Pruebas previas/Andres/js vanila/NLP/js/app.js
+++ Pruebas previas/Andres/js vanila/NLP/js/app.js
@@ -1,33 +1,88 @@
-// document.addEventListener("DOMContentLoaded", function() {
-// const translateBtn = document.getElementById("translateBtn");
-// const inputText = document.getElementById("inputText");
-// const outputDiv = document.getElementById("output");
+// const axios = require("axios").default;
 
-// translateBtn.addEventListener("click", function() {
-//     const textToTranslate = inputText.value;
-//     if (textToTranslate.trim() === "") {
-//         outputDiv.innerHTML = "Por favor, ingresa un texto para traducir.";
-//         return;
-//     }
+const URL_ENDPOINT_TRADUCCION = "https://api.cognitive.microsofttranslator.com";
+const KEY_ENPOINT_TRADUCCION = "65c5f3d11b5d4dfdab331ddda5936ac2";
+const LOCATION = "eastus";
+
+const btn = document.getElementById("translateBtn");
+const input = document.getElementById("inputText");
+const output = document.getElementById("outputText");
+const detect = document.getElementById("selecLanguage");
+const muestra = document.getElementById("dropdownButton");
+const listaIdiomas = document.querySelectorAll(".dropdown-menu li");
+
+listaIdiomas.forEach(function (elemento) {
+  elemento.addEventListener("click", function () {
+    // Obtén el valor del elemento seleccionado
+    var valorSeleccionado = elemento.getAttribute("value");
+
+    // Actualiza el texto del botón con el valor seleccionado
+    const inputW = valorSeleccionado;
+    muestra.innerHTML = `${
+      inputW == "es"
+        ? "Español"
+        : inputW === "en"
+        ? "Ingles"
+        : inputW === "fr"
+        ? "Frances"
+        : inputW === "pt"
+        ? "Portugues"
+        : "otro"
+      }`;
+
     
-//     // Conecta con el endpoint de traducción (reemplaza con tu endpoint real)
-//     const endpoint = "URL_DEL_ENDPOINT_DE_TRADUCCION";
-//     fetch(endpoint, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//             text: textToTranslate
-//         })
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//         const translatedText = data.translatedText;
-//         outputDiv.innerHTML = `<strong>Texto traducido:</strong><br>${translatedText}`;
-//     })
-//     .catch(error => {
-//         outputDiv.innerHTML = "Ocurrió un error al traducir el texto.";
-//     });
-// });
-// });
+    
+    // funcion para traducir
+    btn.addEventListener("click", (algo) => {
+      algo.preventDefault();
+
+      const words = input.value;
+
+      axios({
+        baseURL: URL_ENDPOINT_TRADUCCION,
+        // usar 'detect', para solo detectar el idioma
+        // usar '/dictionary/lookup', para traducciones alternativas
+        url: "/translate",
+        method: "post",
+        headers: {
+          "Ocp-Apim-Subscription-Key": KEY_ENPOINT_TRADUCCION,
+          "Ocp-Apim-Subscription-Region": LOCATION,
+          "Content-type": "application/json",
+        },
+        params: {
+          "api-version": "3.0",
+          //from: inputW, // no incluir para detectar idioma - entre mas cerca a 1 es su seguridad
+          to: inputW, // quitar solo para la deteccion
+          // 'toScript': 'latn', // para la transliteracion fonectica
+          // 'includeSentenceLength': true
+        },
+        data: [
+          {
+            text: words,
+          },
+        ],
+        responseType: "json",
+      })
+        .then(function (response) {
+          const language = response.data[0].detectedLanguage.language;
+          const content = response.data[0].translations[0].text;
+          detect.innerHTML = `${
+            language == "es"
+              ? "Español"
+              : language === "en"
+              ? "Ingles"
+              : language == "fr"
+              ? "Frances"
+              : language === "pt"
+              ? "Portugues"
+              : "otro"
+          }`;
+          output.innerHTML = content;
+          // console.log(JSON.stringify(response.data, null, 4));
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    });
+  });
+});
